93bae65bd2722681e7a08531f5d36ea79fb4eb8d
김찬우 <chanwoo040531@gmail.com>
2024-05-14 15:39:46 +0900

    [feat] create `BaseEntity`

---
+++ src/main/kotlin/me/chnu/replication/domain/BaseEntity.kt
@@ -0,0 +1,29 @@
+package me.chnu.replication.domain
+
+import jakarta.persistence.*
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+) {
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+        if (this.javaClass != other?.javaClass) {
+            return false
+        }
+
+        other as BaseEntity
+
+        return id == other.id
+    }
+}
