44097759223ba1a61721d526472b3263d329d2a5
김찬우 <chanwoo040531@gmail.com>
2024-06-17 18:02:09 +0900

    [feat] testcontainer

--- src/test/kotlin/me/chnu/replication/TestReplicationApplication.kt
+++ src/test/kotlin/me/chnu/replication/TestReplicationApplication.kt
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.boot.with
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.DependsOn
+import org.springframework.core.env.Environment
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
 
@@ -13,10 +15,41 @@ class TestReplicationApplication {
 
     @Bean
     @ServiceConnection
-    fun postgresContainer(): PostgreSQLContainer<*> {
-        return PostgreSQLContainer(DockerImageName.parse("postgres:latest"))
-    }
+    fun primaryContainer(environment: Environment) =
+        PostgreSQLContainer(DockerImageName.parse("postgres:latest")).apply {
+            dockerImageName = PRIMARY_DATASOURCE
 
+            withDatabaseName("postgres_primary")
+            withUsername("postgres")
+            withPassword("password")
+            withCommand(
+                "postgres",
+                "-c", "wal_level=replica",
+                "-c", "hot_standby=on",
+                "-c", "max_wal_senders=10",
+                "-c", "max_replication_slots=10",
+                "-c", "hot_standby_feedback=on",
+            )
+            addParameter("--auth-host", "sha-256")
+            withInitScript("00_init.sql")
+        }
+
+    @Bean
+    @DependsOn(PRIMARY_DATASOURCE)
+    @ServiceConnection
+    fun standbyContainer(environment: Environment) =
+        PostgreSQLContainer(DockerImageName.parse("postgres:latest")).apply {
+            dockerImageName = STANDBY_DATASOURCE
+            withDatabaseName("postgres_standby")
+            withUsername("postgres")
+            withPassword("password")
+
+        }
+
+    companion object {
+        const val PRIMARY_DATASOURCE = "primaryContainer"
+        const val STANDBY_DATASOURCE = "standbyContainer"
+    }
 }
 
 fun main(args: Array<String>) {
