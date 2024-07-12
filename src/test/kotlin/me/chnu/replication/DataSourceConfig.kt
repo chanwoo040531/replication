406939ad88ae53c733415fa2d02bccfc9605de53
김찬우 <chanwoo040531@gmail.com>
2024-07-12 18:02:30 +0900

    [feat] testcontainers

--- src/test/kotlin/me/chnu/replication/DataSourceConfig.kt
+++ src/test/kotlin/me/chnu/replication/DataSourceConfig.kt
@@ -54,13 +54,13 @@ class DataSourceConfig {
             .withEnv("PGPASSWORD", "replicator_password")
             .withCommand(
                 "bash -c",
-                "until pg_basebackup --pgdata=/var/lib/postgresql/data -R --slot=replication_slot --host=primary --port=5432",
+                "until pg_basebackup --pgdata=/var/lib/postgresql/data -R --slot=replication_slot --host=primary --port=5432;",
                 "do",
-                "echo 'Waiting for primary to connect...'",
-                "sleep 1s",
-                "done",
+                "echo 'Waiting for primary to connect...';",
+                "sleep 1s;",
+                "done;",
                 "echo 'Backup done, starting replica...'",
-                "chmod 0700 /var/lib/postgresql/data",
+                "chmod 0700 /var/lib/postgresql/data;",
                 "postgres",
             )
             .withReuse(true)
